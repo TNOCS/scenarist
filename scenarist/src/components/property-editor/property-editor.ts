ce3e6d43f0412e8a661242179b695d596f0aef6f
Erik Vullings <erik.vullings@gmail.com>
2017-10-11 17:35:24 +0200

    Property editor is working.

--- scenarist/src/components/property-editor/property-editor.ts
+++ scenarist/src/components/property-editor/property-editor.ts
@@ -1,19 +1,21 @@
 import { IPropertyType } from './../../models/property';
 import { inject } from 'aurelia-framework';
 import { Endpoint, Rest } from 'aurelia-api';
-import 'fetch';
+// import 'fetch';
+import { MdToastService } from 'aurelia-materialize-bridge';
 
-@inject(Endpoint.of('db'))
+@inject(Endpoint.of('db'), MdToastService)
 export class PropertyEditorCustomElement {
   public properties: IPropertyType[];
   public activeProperty: IPropertyType;
   public showPropertyEditor = false;
 
   private rest: Rest;
+  private toast;
 
-  constructor(rest) {
+  constructor(rest: Rest, toast) {
     this.rest = rest;
-    console.log(this.rest.defaults);
+    this.toast = toast;
   }
 
   public attached() {
@@ -35,21 +37,38 @@ export class PropertyEditorCustomElement {
   }
 
   public deleteProperty() {
-    console.log('Delete property clicked');
+    const index = this.properties.indexOf(this.activeProperty);
+    if (index < 0) { return console.warn('Cannot find property! Ignoring.'); }
+    this.rest
+      .destroy('properties', this.activeProperty.id)
+      .then(() => {
+        this.properties.splice(index, 1);
+        this.activeProperty = null;
+        this.showPropertyEditor = false;
+        this.toastMessage('Delete successfull.');
+      })
+      .catch(m => this.toastMessage('Error deleting property!\n' + m, true));
   }
 
   public saveProperty() {
     if (this.activeProperty.id) {
       this.rest
         .update('properties', this.activeProperty.id, this.activeProperty)
-        .then(console.log)
-        .catch(console.error);
+        .then(() => this.toastMessage('Update successfull.'))
+        .catch(m => this.toastMessage('Error updating property!\n' + m, true));
     } else {
       this.rest
         .post('properties', this.activeProperty)
-        .then(prop => this.activeProperty = prop)
-        .catch(console.error);
+        .then(prop => {
+          this.activeProperty = prop;
+          this.toastMessage('Created successfully.');
+        })
+        .catch(m => this.toastMessage('Error creating property!\n' + m, true));
     }
+  }
+
+  private toastMessage(msg: string, isError = false) {
+    this.toast.show(msg, isError ? 2000 : 4000, isError ? 'red' : 'green');
   }
 
 }
