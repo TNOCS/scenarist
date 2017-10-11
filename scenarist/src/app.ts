f06ee60b87323f108017615e07edab4516be6c49
Erik Vullings <erik.vullings@gmail.com>
2017-10-11 19:41:16 +0200

    New property editor page.

--- scenarist/src/app.ts
+++ scenarist/src/app.ts
@@ -10,7 +10,8 @@ export class App {
     config.map([
       { route: ['', 'welcome'], name: 'welcome', moduleId: PLATFORM.moduleName('./components/home/home'), nav: true, title: 'Home' },
       { route: ['scenario'], name: 'scenario', moduleId: PLATFORM.moduleName('./components/scenario-editor/scenario-editor'), nav: true, title: 'Scenario' },
-      { route: ['entities'], name: 'entities', moduleId: PLATFORM.moduleName('./components/entity-editor/entity-editor'), nav: true, title: 'Entity' }
+      { route: ['entities'], name: 'entities', moduleId: PLATFORM.moduleName('./components/entity-manager/entity-manager'), nav: true, title: 'Entities' },
+      { route: ['properties'], name: 'properties', moduleId: PLATFORM.moduleName('./components/property-editor/property-editor'), nav: true, title: 'Properties' }
     ]);
   }
 
