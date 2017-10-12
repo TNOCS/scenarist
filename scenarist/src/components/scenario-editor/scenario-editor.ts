5180daf7c2ab167cdbca52a0c973509ea3de0c93
Erik Vullings <erik.vullings@gmail.com>
2017-10-12 21:55:39 +0200

    Added leaflet control to scenario page.

--- scenarist/src/components/scenario-editor/scenario-editor.ts
+++ scenarist/src/components/scenario-editor/scenario-editor.ts
@@ -1,3 +1,22 @@
 export class ScenarioEditorCustomElement {
-  public title = 'Scenario Editor';
+  public attached() {
+    const mapMargin = 65;
+    const map = $('#map');
+    const w = $(window);
+    // https://gis.stackexchange.com/questions/62491/sizing-leaflet-map-inside-bootstrap
+    const resize = () => {
+      const height = w.height();
+      map.css('height', height - mapMargin);
+      // map.css('margin-top', 50);
+      // if (w.width() >= 980) {
+      //   map.css('height', height - mapMargin);
+      //   map.css('margin-top', 50);
+      // } else {
+      //   map.css('height', height - (mapMargin + 12));
+      //   map.css('margin-top', -21);
+      // }
+    };
+    w.on('resize', resize);
+    resize();
+  }
 }
