8c73f7598ff9e0955019b31bdb51d300912b7d94
Erik Vullings <erik.vullings@gmail.com>
2017-10-12 19:53:35 +0200

    Added events for loading.

--- scenarist/src/models/state.ts
+++ scenarist/src/models/state.ts
@@ -1,17 +1,18 @@
+import { EventAggregator } from 'aurelia-event-aggregator';
 import { inject } from 'aurelia-framework';
 import { IProperty } from './property';
 import { IEntityType, IEntity } from './entity';
 import { Endpoint, Rest } from 'aurelia-api';
 
-@inject(Endpoint.of('db'))
+@inject(Endpoint.of('db'), EventAggregator)
 export class State {
   public entityTypes: IEntityType[] = [];
   public entities: IEntity[] = [];
   public properties: IProperty[] = [];
 
-  constructor(private rest: Rest) {
-    this.rest.find('properties').then(pt => this.properties = pt);
-    this.rest.find('entityTypes').then(et => this.entityTypes = et);
-    this.rest.find('entities').then(et => this.entities = et);
+  constructor(private rest: Rest, private ea: EventAggregator) {
+    this.rest.find('properties').then(pt => this.properties = pt).then(() => ea.publish('propertiesUpdated'));
+    this.rest.find('entityTypes').then(et => this.entityTypes = et).then(() => ea.publish('entityTypesUpdated'));
+    this.rest.find('entities').then(et => this.entities = et).then(() => ea.publish('entitiesUpdated'));
   }
 }
