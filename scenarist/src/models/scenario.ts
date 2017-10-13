ec30eef93725377e44552643486874b1dd9576cc
Erik Vullings <erik.vullings@gmail.com>
2017-10-13 19:06:53 +0200

    IScenario: added extra props, center and zoom.

--- scenarist/src/models/scenario.ts
+++ scenarist/src/models/scenario.ts
@@ -1,8 +1,10 @@
 import { IModel } from './model';
 
 export interface IScenario extends IModel {
-  start?: { data: Date; time: Date }
-  end?: { data: Date; time: Date }
+  start?: { data: Date; time: Date };
+  end?: { data: Date; time: Date };
+  center?: { lat: number; lng: number; };
+  zoom?: number;
   trackIds?: string[] | number[];
   layers?: {
     baseIds?;
