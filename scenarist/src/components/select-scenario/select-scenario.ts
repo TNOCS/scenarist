4455d5498638f3a6443aa4a74af3781bc69f4b22
Erik Vullings <erik.vullings@gmail.com>
2017-10-13 10:06:23 +0200

    Added scenario metadata editor

---
+++ scenarist/src/components/select-scenario/select-scenario.ts
@@ -0,0 +1,42 @@
+import { IScenario } from './../../models/scenario';
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { State, ModelType } from './../../models/state';
+import { inject } from 'aurelia-framework';
+
+@inject(State, EventAggregator)
+export class ScenarioEditorCustomElement {
+  public scenarios: IScenario[];
+  public activeScenario: IScenario;
+  public showScenarioSelector = false;
+  private modelType: ModelType = 'scenarios';
+
+  constructor(private state: State, private ea: EventAggregator) {}
+
+  public attached() {
+    this.scenarios = this.state.scenarios;
+    this.ea.subscribe(`${this.modelType}Updated`, (scenario: IScenario) => {
+      this.scenarios = this.state.scenarios;
+      if (scenario) { this.activeScenario = scenario; }
+    });
+  }
+
+  public selectScenario(selected: IScenario) {
+    this.activeScenario = this.activeScenario === selected ? null : selected;
+    this.showScenarioSelector = this.activeScenario ? true : null;
+  }
+
+  public addScenario() {
+    this.activeScenario = {} as IScenario;
+    this.showScenarioSelector = true;
+    this.state.save(this.modelType, this.activeScenario);
+  }
+
+  public deleteScenario() {
+    this.showScenarioSelector = false;
+    this.state.delete(this.modelType, this.activeScenario);
+  }
+
+  public saveScenario() {
+    this.state.save(this.modelType, this.activeScenario);
+  }
+}
