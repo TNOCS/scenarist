c0e79b8c725b2c18eef2fb833c99faa5a4f53449
Erik Vullings <erik.vullings@gmail.com>
2017-10-13 10:05:58 +0200

    Refactored state model. Only returns copies of data in store, all changes need to go through the state module's functions.

--- scenarist/src/components/entity-type-editor/entity-type-editor.ts
+++ scenarist/src/components/entity-type-editor/entity-type-editor.ts
@@ -1,23 +1,23 @@
 import { EventAggregator } from 'aurelia-event-aggregator';
 import { IEntityType } from './../../models/entity';
-import { State } from './../../models/state';
+import { State, ModelType } from './../../models/state';
 import { inject } from 'aurelia-framework';
-import { Endpoint, Rest } from 'aurelia-api';
-// import 'fetch';
-import { MdToastService } from 'aurelia-materialize-bridge';
 
-@inject(State, Endpoint.of('db'), MdToastService, EventAggregator)
+@inject(State, EventAggregator)
 export class EntityTypeEditorCustomElement {
   public entityTypes: IEntityType[];
   public activeEntityType: IEntityType;
   public showEntityTypeEditor = false;
-  private api = 'entityTypes';
+  private modelType: ModelType = 'entityTypes';
 
-  constructor(private state: State, private rest: Rest, private toast: MdToastService, private ea: EventAggregator) {}
+  constructor(private state: State, private ea: EventAggregator) {}
 
   public attached() {
     this.entityTypes = this.state.entityTypes;
-    this.ea.subscribe('entityTypesUpdated', () => this.entityTypes = this.state.entityTypes);
+    this.ea.subscribe(`${this.modelType}Updated`, (et: IEntityType) => {
+      this.entityTypes = this.state.entityTypes;
+      if (et) { this.activeEntityType = et; }
+    });
   }
 
   public selectEntityType(selected: IEntityType) {
@@ -28,42 +28,16 @@ export class EntityTypeEditorCustomElement {
   public addEntityType() {
     this.activeEntityType = {} as IEntityType;
     this.showEntityTypeEditor = true;
-    this.entityTypes.push(this.activeEntityType);
+    this.state.save(this.modelType, this.activeEntityType);
   }
 
   public deleteEntityType() {
-    const index = this.entityTypes.indexOf(this.activeEntityType);
-    if (index < 0) { return console.warn('Cannot find entity type! Ignoring.'); }
-    this.rest
-      .destroy(this.api, this.activeEntityType.id)
-      .then(() => {
-        this.entityTypes.splice(index, 1);
-        this.activeEntityType = null;
-        this.showEntityTypeEditor = false;
-        this.toastMessage('Delete successfull.');
-      })
-      .catch(m => this.toastMessage('Error deleting entity type!\n' + m, true));
+    this.showEntityTypeEditor = false;
+    this.state.delete(this.modelType, this.activeEntityType);
   }
 
   public saveEntityType() {
-    if (this.activeEntityType.id) {
-      this.rest
-        .update(this.api, this.activeEntityType.id, this.activeEntityType)
-        .then(() => this.toastMessage('Update successfull.'))
-        .catch(m => this.toastMessage('Error updating entity type!\n' + m, true));
-    } else {
-      this.rest
-        .post(this.api, this.activeEntityType)
-        .then(prop => {
-          this.activeEntityType = prop;
-          this.toastMessage('Created successfully.');
-        })
-        .catch(m => this.toastMessage('Error creating entity type!\n' + m, true));
-    }
-  }
-
-  private toastMessage(msg: string, isError = false) {
-    this.toast.show(msg, isError ? 2000 : 4000, isError ? 'red' : 'green');
+    this.state.save(this.modelType, this.activeEntityType);
   }
 
 }
