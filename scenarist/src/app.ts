27f80dd5c816d44217feb7f02dd1b9dc1395d7c8
Erik Vullings <erik.vullings@gmail.com>
2017-10-13 16:55:05 +0200

    Added route to layer editor.

--- scenarist/src/app.ts
+++ scenarist/src/app.ts
@@ -11,7 +11,8 @@ export class App {
       { route: ['', 'home'], name: 'welcome', moduleId: PLATFORM.moduleName('./components/scenario-metadata-editor/scenario-metadata-editor'), nav: true, title: 'Home' },
       { route: ['scenario'], name: 'scenario', moduleId: PLATFORM.moduleName('./components/scenario-editor/scenario-editor'), nav: true, title: 'Scenario' },
       { route: ['entities'], name: 'entities', moduleId: PLATFORM.moduleName('./components/entity-type-editor/entity-type-editor'), nav: true, title: 'Entities' },
-      { route: ['properties'], name: 'properties', moduleId: PLATFORM.moduleName('./components/property-editor/property-editor'), nav: true, title: 'Properties' }
+      { route: ['properties'], name: 'properties', moduleId: PLATFORM.moduleName('./components/property-editor/property-editor'), nav: true, title: 'Properties' },
+      { route: ['layers/:layertype'], name: 'layers', moduleId: PLATFORM.moduleName('./components/layer-editor/layer-editor'), nav: false, title: 'Layers' }
     ]);
   }
 
