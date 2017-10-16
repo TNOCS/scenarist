83db15a47f4b75ae3a5de62cee1f1c5434bd6ff0
rinze <rinze.bruining@tno.nl>
2017-10-16 14:22:47 +0200

    Clean copy-pasted code in player-gui. Add dummy play/pause/stop controls

--- scenarist/src/components/player-controls/player-controls.ts
+++ scenarist/src/components/player-controls/player-controls.ts
@@ -1,12 +1,10 @@
 import { EventAggregator } from 'aurelia-event-aggregator';
-import { State, ModelType } from './../../models/state';
-import { IProperty } from './../../models/property';
 import { inject } from 'aurelia-framework';
 
-@inject(State, EventAggregator)
+@inject(EventAggregator)
 export class PlayerControlsCustomElement {
 
-  constructor(private state: State, private ea: EventAggregator) {}
+  constructor(private ea: EventAggregator) {}
 
   public attached() {
       
--- scenarist/src/components/player-gui/player-gui.ts
+++ scenarist/src/components/player-gui/player-gui.ts
@@ -1,42 +1,14 @@
 import { EventAggregator } from 'aurelia-event-aggregator';
-import { State, ModelType } from './../../models/state';
-import { IProperty } from './../../models/property';
 import { inject } from 'aurelia-framework';
 
-@inject(State, EventAggregator)
+@inject(EventAggregator)
 export class PlayerGUI {
-  public properties: IProperty[];
-  public activeProperty: IProperty;
-  public showPropertyEditor = false;
-  private modelType: ModelType = 'properties';
+  public showControls = true;
 
-  constructor(private state: State, private ea: EventAggregator) {}
+  constructor(private ea: EventAggregator) {}
 
   public attached() {
-    this.properties = this.state.properties;
-    this.ea.subscribe(`${this.modelType}Updated`, (prop: IProperty) => {
-      this.properties = this.state.properties;
-      if (prop) { this.activeProperty = this.properties.filter(p => p.id === prop.id)[0]; }
-    });
+    
   }
-
-  public selectProperty(selected: IProperty) {
-    this.activeProperty = this.activeProperty === selected ? null : selected;
-    this.showPropertyEditor = this.activeProperty ? true : null;
-  }
-
-  public addProperty() {
-    this.activeProperty = {} as IProperty;
-    this.showPropertyEditor = true;
-    this.state.save(this.modelType, this.activeProperty);
-  }
-
-  public deleteProperty() {
-    this.showPropertyEditor = false;
-    this.state.delete(this.modelType, this.activeProperty);
-  }
-
-  public saveProperty() {
-    this.state.save(this.modelType, this.activeProperty);
-  }
+  
 }
