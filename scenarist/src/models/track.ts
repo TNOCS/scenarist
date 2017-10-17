29bcc70c320cb26b7fb98eda31d16379ef665f96
Erik Vullings <erik.vullings@gmail.com>
2017-10-17 22:53:47 +0200

    Added ITrackView for the viewmodel

--- scenarist/src/models/track.ts
+++ scenarist/src/models/track.ts
@@ -4,3 +4,7 @@ export interface ITrack extends IModel {
   entityTypeId: string | number;
   features: Array<GeoJSON.Feature<GeoJSON.Point>>;
 }
+
+export interface ITrackView extends ITrack {
+  isVisible: boolean;
+}
