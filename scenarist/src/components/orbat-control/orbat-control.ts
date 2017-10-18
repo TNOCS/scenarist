cb893706b484b77e18cc857b33278117990528b5
Erik Vullings <erik.vullings@gmail.com>
2017-10-18 12:16:34 +0200

    Bindable tracks

--- scenarist/src/components/orbat-control/orbat-control.ts
+++ scenarist/src/components/orbat-control/orbat-control.ts
@@ -1,25 +1,18 @@
 import { IScenario } from './../../models/scenario';
 import { EventAggregator } from 'aurelia-event-aggregator';
 import { State } from './../../models/state';
-import { autoinject } from 'aurelia-framework';
+import { autoinject, bindable } from 'aurelia-framework';
 import { ITrackView } from 'models/track';
 
 @autoinject
 export class OrbatControlCustomElement {
+  @bindable public tracks: ITrackView[];
   public scenario: IScenario;
-  public tracks: ITrackView[];
 
   constructor(private state: State, private ea: EventAggregator) {}
 
   public attached() {
     this.scenario = this.state.scenario;
-    if (!this.scenario) { return; }
-    this.tracks = this.state.tracks
-      .filter(t => this.scenario.trackIds.includes(t.id as number))
-      .map((t: ITrackView) => {
-        t.isVisible = true;
-        return t;
-      });
   }
 
   public toggleVisibility(track: ITrackView) {
