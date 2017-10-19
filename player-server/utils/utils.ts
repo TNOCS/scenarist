bf9dd6a96e00977243b6f2a1cf5ddd6289236114
rinze <rinze.bruining@tno.nl>
2017-10-19 22:47:08 +0200

    Player obtains tracks and accompanying entityTypes, and merges + returns them as a FeatureCollection.

--- player-server/utils/utils.ts
+++ player-server/utils/utils.ts
@@ -1,8 +1,26 @@
-
 export function dateTimeToMillis(date: string, time: string) {
     if (!date || !time) return null;
     let longTimeString = time.concat(':00.000Z');
     let dateString = date.split('T').shift();
     let totalString = dateString + 'T' + longTimeString;
     return Date.parse(totalString);
+}
+
+export function createFeatureCollection() {
+    return {
+        type: "FeatureCollection",
+        features: []
+    };
+}
+
+export function createPointFeature(lon: number, lat: number, properties?: any) {
+    var gjson = {
+        type: "Feature",
+        geometry: {
+            type: "Point",
+            coordinates: [lon, lat]
+        },
+        properties: properties
+    }
+    return gjson;
 }
