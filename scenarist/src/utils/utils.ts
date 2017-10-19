061f69d88cfaa7a38ac5386f463f0975497430a3
Erik Vullings <erik.vullings@gmail.com>
2017-10-19 23:22:30 +0200

    Added @timeout() decorator.

--- scenarist/src/utils/utils.ts
+++ scenarist/src/utils/utils.ts
@@ -1,1 +1,18 @@
 export const clone = (model: any) => { return JSON.parse(JSON.stringify(model)); };
+
+/**
+ * Timeout decorator for methods
+ * @see https://medium.com/front-end-hacking/javascript-make-your-code-cleaner-with-decorators-d34fc72af947
+ * @param milliseconds
+ */
+export const timeout = (milliseconds = 0) => {
+  return function (target, key, descriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args) {
+      setTimeout(() => {
+        originalMethod.apply(this, args);
+      }, milliseconds);
+    };
+    return descriptor;
+  };
+};
