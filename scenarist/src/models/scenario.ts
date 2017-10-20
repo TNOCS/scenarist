29377e5f1b5fb4db7e76c7f85d54f2e688359558
Erik Vullings <erik.vullings@gmail.com>
2017-10-20 14:24:51 +0200

    Added scenario.simTitle, which is the name to use in the NVG-service.

--- scenarist/src/models/scenario.ts
+++ scenarist/src/models/scenario.ts
@@ -2,6 +2,13 @@ import { ITrack } from './track';
 import { IModel, IdType } from './model';
 
 export interface IScenario extends IModel {
+  /**
+   * Name given to an external service when playing the scenario.
+   *
+   * @type {string}
+   * @memberof IScenario
+   */
+  simTitle?: string;
   start?: { date: Date; time: Date };
   end?: { date: Date; time: Date };
   center?: { lat: number; lng: number; };
