2d4aa2b1e994c4e80602727299b8c4ce65a48962
Erik Vullings <erik.vullings@gmail.com>
2017-10-23 22:10:57 +0200

    The red active time displays the actual time. Keyframe markers indicate how many markers are present.

--- scenarist/src/resources/elements/aurelia-d3-timeline/aurelia-d3-timeline.ts
+++ scenarist/src/resources/elements/aurelia-d3-timeline/aurelia-d3-timeline.ts
@@ -12,6 +12,23 @@ export interface IKeyframe {
   }[];
 }
 
+export interface IKeyframeFormat {
+  /**
+   * Keyframe start time
+   *
+   * @type {Date}
+   * @memberof IKeyframeFormat
+   */
+  start: Date;
+  /**
+   * Keyframe labels
+   *
+   * @type {string[]}
+   * @memberof IKeyframeFormat
+   */
+  label: string[];
+}
+
 @autoinject
 export class AureliaD3TimelineCustomElement {
   public title = 'Timeline';
@@ -20,22 +37,22 @@ export class AureliaD3TimelineCustomElement {
   @bindable public startTime: string;
   @bindable public endTime: string;
   @bindable public keyframes: IKeyframe[];
+  @bindable public currentTime: Date;
 
   private isInitialized = false;
-  private svg: any;
-  private tl: any;
-  private x: any;
-  private xZoomed: any;
+  private svg: Selection<SVGElement, IKeyframeFormat[], HTMLDivElement, IKeyframe[]>;
+  private tl: Selection<SVGElement, IKeyframeFormat[], HTMLDivElement, IKeyframe[]>;
+  private x: d3.ScaleTime<Date, number>;
+  private xZoomed: d3.ScaleTime<any, number>;
   private gX: any;
-  private xAxis: any;
-  private line: any;
-  private tooltip: any;
+  private xAxis: d3.Axis<Date>;
+  private line: Selection<any, any, any, any>;
+  private curTimeline: Selection<any, any, any, any>;
+  private curTimeLabel: Selection<any, any, any, any>;
+  private tooltip: Selection<HTMLDivElement, any, HTMLBodyElement, any>;
+  private toTime: (x: number) => string;
 
   constructor(private el: Element) { }
-
-  // public created() {
-  //   console.log('created');
-  // }
 
   public attached() {
     this.drawBase();
@@ -47,10 +64,10 @@ export class AureliaD3TimelineCustomElement {
   }
 
   private initialize() {
-    const margin = { top: 20, bottom: 20, left: 20, right: 20 };
+    const margin = { top: 10, bottom: 10, left: 20, right: 20 };
     const width = +this.width - margin.left - margin.right;
     const height = +this.height - margin.top - margin.bottom;
-    const timelinePos = height - 20;
+    const timelinePos = height - 10;
     return { margin, width, height, timelinePos };
   }
 
@@ -64,8 +81,8 @@ export class AureliaD3TimelineCustomElement {
     this.svg = d3
       .select(this.el)
       .append('svg')
-      .attr('width', +this.width)
-      .attr('height', +this.height);
+      .attr('width', +this.width || 900)
+      .attr('height', +this.height || 70) as Selection<SVGAElement, IKeyframeFormat[], HTMLDivElement, IKeyframe[]>;
 
     const extraRange = Math.round((endTime - startTime) / 40);
     this.x = d3.scaleTime()
@@ -76,7 +93,7 @@ export class AureliaD3TimelineCustomElement {
     this.svg.append('clipPath')
       .attr('id', 'rect-clip')
       .append('rect')
-      .attr('x', 0)
+      .attr('x', margin.left)
       .attr('y', margin.top)
       .attr('width', width)
       .attr('height', height)
@@ -108,29 +125,41 @@ export class AureliaD3TimelineCustomElement {
 
     this.gX = this.tl.append('g')
       .attr('class', 'axis axis-x')
-      .attr('transform', `translate(0, ${height})`)
+      .attr('transform', `translate(0, ${height - 10})`)
       .call(this.xAxis);
 
+    const formatCurTime = d3.timeFormat('%a %H:%M:%S');
+    this.toTime = (x: number): string => formatCurTime(this.xZoomed.invert(x));
     const drag = d3.drag()
       // .on('dragstart', null)
-      .on('drag', (d) => {
+      .on('drag', () => {
         const dx = currentEvent.dx; // this will give the delta x moved by drag
-        const x1New = +curTimeline.attr('x1') + dx;
-        const x2New = +curTimeline.attr('x2') + dx;
-        curTimeline
-          .attr('x1', x1New)
-          .attr('x2', x2New);
+        const xNew = +this.curTimeline.attr('x1') + dx;
+        this.currentTime = this.xZoomed.invert(xNew);
+        this.curTimeline
+          .attr('x1', xNew)
+          .attr('x2', xNew);
+        this.curTimeLabel
+          .attr('x', xNew + 10)
+          .text(() => this.toTime(xNew));
       });
-    // .on('dragend', null);
 
-    const curTimeline = this.tl
+    this.curTimeline = this.tl
       .append('line')
       .attr('class', 'active-time')
       .attr('x1', Math.round(width / 2))
       .attr('y1', timelinePos - 30)
       .attr('x2', Math.round(width / 2))
-      .attr('y2', timelinePos + 10)
+      .attr('y2', timelinePos)
       .call(drag);
+
+    const xStart = Math.round(width / 2) + 10;
+    this.curTimeLabel = this.tl
+      .append('text')
+      .attr('x', xStart)
+      .attr('y', timelinePos - 18)
+      .text(() => this.toTime(xStart));
+    this.currentTime = this.xZoomed.invert(xStart);
 
     this.isInitialized = true;
   }
@@ -166,91 +195,35 @@ export class AureliaD3TimelineCustomElement {
       }
       return p;
     }, {});
-    const dc: Array<{ start: Date, label: string[] }> = [];
+    const dc: IKeyframeFormat[] = [];
     for (const key in grouped) {
       if (!grouped.hasOwnProperty(key)) { continue; }
       dc.push({ start: new Date(+key), label: grouped[key].labels });
     }
 
-    const earliest = (times: Array<{ start: Date }>) => times.reduce((p, c) => p.start < c.start ? p : c).start;
-    const latest = (times: Array<{ start: Date, end?: Date }>) => {
-      const t = times.reduce((p, c) => (p.end || p.start) > (c.end || c.start) ? p : c);
-      return t.end || t.start;
-    };
-    const startTime = this.startTime
-      ? Date.parse(this.startTime)
-      : data.reduce((p, c) => {
-        const t = earliest(c.times);
-        return p < t ? p : t;
-      }, earliest(data[0].times)).valueOf();
-    const endTime = this.endTime
-      ? Date.parse(this.endTime)
-      : data.reduce((p, c) => {
-        const t = latest(c.times);
-        return p < t ? p : t;
-      }, latest(data[0].times)).valueOf();
-
     const { margin, width, height, timelinePos } = this.initialize();
 
-    // const resetted = () => {
-    //   this.svg.transition()
-    //     .duration(750)
-    //     .call(zoom.transform, d3.zoomIdentity);
-    // };
-
-    // d3.select(this.el).select('svg').remove();
-    // const svg = d3
-    //   .select(this.el)
-    //   .append('svg')
-    //   .attr('width', +this.width)
-    //   .attr('height', +this.height);
-
-    // const extraRange = Math.round((endTime - startTime) / 40);
-    // const x = d3.scaleTime()
-    //   .domain([startTime - extraRange, endTime + extraRange])
-    //   .range([0, width]);
-
-    // svg.append('clipPath')
-    //   .attr('id', 'rect-clip')
-    //   .append('rect')
-    //   .attr('x', 0)
-    //   .attr('y', margin.top)
-    //   .attr('width', width)
-    //   .attr('height', height)
-    //   .attr('clip-path', 'url(#ellipse-clip)')
-    //   .attr('fill', 'SteelBlue');
-
-    // const tl = svg
-    //   .append('g')
-    //   .attr('transform', `translate(${margin.left}, ${margin.top})`)
-    //   .attr('clip-path', 'url(#rect-clip)') as Selection<SVGElement, any, HTMLDivElement, any>;
-
-    // const line = tl
-    //   .append('line')
-    //   .attr('class', 'timeline')
-    //   .attr('x1', 0)
-    //   .attr('y1', timelinePos)
-    //   .attr('x2', width)
-    //   .attr('y2', timelinePos);
-
-    // // Define the div for the tooltip
-    // const tooltip = d3.select('body')
-    //   .append('div')
-    //   .attr('class', 'tooltip')
-    //   .style('opacity', 0);
-
     const formatTime = d3.timeFormat('%H:%M:%S');
-    d3.selectAll('circle').remove();
+    d3.select('#markers').remove();
     const markers = this.tl
       .append('g')
+      .attr('id', 'markers')
       .selectAll('circle')
       .data(dc)
-      .enter()
+      .enter();
+    const circles = markers
       .append('circle')
       .attr('class', 'circle')
       .attr('r', 10)
       .attr('cx', d => this.xZoomed(d.start))
-      .attr('cy', timelinePos)
+      .attr('cy', timelinePos);
+    markers
+      .append('text')
+      .attr('x', d => this.xZoomed(d.start))
+      .attr('y', timelinePos + 5) // 5 == half font height
+      .attr('text-anchor', 'middle')
+      .text(d => d.label.length < 10 ? d.label.length : '*');
+    circles
       .on('mouseover', d => {
         this.tooltip.transition()
           .duration(200)
@@ -265,25 +238,6 @@ export class AureliaD3TimelineCustomElement {
           .style('opacity', 0);
       });
 
-    // this.xAxis = d3.axisBottom(this.x)
-    //   .ticks(width / 100)
-    //   .tickFormat(this.multiTimeFormat);
-
-    // // const view = svg.append('rect')
-    // //   .attr('class', 'view')
-    // //   .attr('x', 0.5)
-    // //   .attr('y', 0.5)
-    // //   .attr('width', width - 1)
-    // //   .attr('height', height - 1);
-
-    // const gX = this.tl.append('g')
-    //   .attr('class', 'axis axis-x')
-    //   .attr('transform', `translate(0, ${height})`)
-    //   .call(this.xAxis);
-
-    // d3.select('button')
-    //   .on('click', resetted);
-
     const zoomed = () => {
       // view.attr('transform', currentEvent.transform);
       this.xZoomed = currentEvent.transform.rescaleX(this.x);
@@ -291,11 +245,24 @@ export class AureliaD3TimelineCustomElement {
       // markers.attr('transform', `translate(${currentEvent.transform.x}, 0) scale(${currentEvent.transform.k}, 1)`);
       this.svg
         .selectAll('circle')
-        .attr('cx', d => this.xZoomed(d.start));
+        .attr('cx', (d: IKeyframeFormat) => this.xZoomed(d.start));
+
+      this.svg
+        .select('#markers')
+        .selectAll('text')
+        .attr('x', (d: IKeyframeFormat) => this.xZoomed(d.start));
+
+      // Update the position of the curTimeLine, such that, in principle, the display time remains the same when zooming.
+      const xNew = Math.max(margin.left, Math.min(width, this.xZoomed(this.currentTime)));
+      this.currentTime = this.xZoomed.invert(xNew);
+      this.curTimeline
+        .attr('x1', xNew)
+        .attr('x2', xNew);
+      this.curTimeLabel
+        .attr('x', xNew + 10)
+        .text(() => this.toTime(xNew));
     };
     const zoom = d3.zoom()
-      // .scaleExtent([1, 40])
-      // .translateExtent([[-100, -100], [width + 90, height + 100]])
       .on('zoom', zoomed) as ZoomBehavior<SVGElement, any>;
 
     this.svg.call(zoom);
