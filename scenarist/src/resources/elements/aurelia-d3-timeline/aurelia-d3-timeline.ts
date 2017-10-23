32918d1e4484b449cf5e659626ce817e30e7cfec
Erik Vullings <erik.vullings@gmail.com>
2017-10-23 14:58:40 +0200

    Maintain the zoom settings when adding/removing new keyframes. Does not yet automatically update the keyframes when the title has changed, or a keyframe is added.

--- scenarist/src/resources/elements/aurelia-d3-timeline/aurelia-d3-timeline.ts
+++ scenarist/src/resources/elements/aurelia-d3-timeline/aurelia-d3-timeline.ts
@@ -21,6 +21,16 @@ export class AureliaD3TimelineCustomElement {
   @bindable public endTime: string;
   @bindable public keyframes: IKeyframe[];
 
+  private isInitialized = false;
+  private svg: any;
+  private tl: any;
+  private x: any;
+  private xZoomed: any;
+  private gX: any;
+  private xAxis: any;
+  private line: any;
+  private tooltip: any;
+
   constructor(private el: Element) { }
 
   // public created() {
@@ -36,12 +46,76 @@ export class AureliaD3TimelineCustomElement {
     this.draw();
   }
 
-  private drawBase() {
+  private initialize() {
+    const margin = { top: 20, bottom: 20, left: 20, right: 20 };
+    const width = +this.width - margin.left - margin.right;
+    const height = +this.height - margin.top - margin.bottom;
+    const timelinePos = height - 20;
+    return { margin, width, height, timelinePos };
+  }
 
+  private drawBase() {
+    if (!this.startTime || !this.endTime) { return; }
+    const startTime = Date.parse(this.startTime);
+    const endTime = Date.parse(this.endTime);
+    const { margin, width, height, timelinePos } = this.initialize();
+
+    d3.select(this.el).select('svg').remove();
+    this.svg = d3
+      .select(this.el)
+      .append('svg')
+      .attr('width', +this.width)
+      .attr('height', +this.height);
+
+    const extraRange = Math.round((endTime - startTime) / 40);
+    this.x = d3.scaleTime()
+      .domain([startTime - extraRange, endTime + extraRange])
+      .range([0, width]);
+    this.xZoomed = this.x;
+
+    this.svg.append('clipPath')
+      .attr('id', 'rect-clip')
+      .append('rect')
+      .attr('x', 0)
+      .attr('y', margin.top)
+      .attr('width', width)
+      .attr('height', height)
+      .attr('clip-path', 'url(#ellipse-clip)')
+      .attr('fill', 'SteelBlue');
+
+    this.tl = this.svg
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+      .attr('clip-path', 'url(#rect-clip)') as Selection<SVGElement, any, HTMLDivElement, any>;
+
+    this.line = this.tl
+      .append('line')
+      .attr('class', 'timeline')
+      .attr('x1', 0)
+      .attr('y1', timelinePos)
+      .attr('x2', width)
+      .attr('y2', timelinePos);
+
+    // Define the div for the tooltip
+    this.tooltip = d3.select('body')
+      .append('div')
+      .attr('class', 'tooltip')
+      .style('opacity', 0);
+
+    this.xAxis = d3.axisBottom(this.x)
+      .ticks(width / 100)
+      .tickFormat(this.multiTimeFormat);
+
+    this.gX = this.tl.append('g')
+      .attr('class', 'axis axis-x')
+      .attr('transform', `translate(0, ${height})`)
+      .call(this.xAxis);
+
+    this.isInitialized = true;
   }
 
   private draw() {
-    if (!this.startTime || !this.endTime) { return; }
+    if (!this.isInitialized) { return; }
     // if (!this.keyframes || this.keyframes.length === 0) { return; }
     const data = this.keyframes;
     const convert = (d: {
@@ -95,60 +169,58 @@ export class AureliaD3TimelineCustomElement {
         return p < t ? p : t;
       }, latest(data[0].times)).valueOf();
 
+    const { margin, width, height, timelinePos } = this.initialize();
+
     const resetted = () => {
-      svg.transition()
+      this.svg.transition()
         .duration(750)
         .call(zoom.transform, d3.zoomIdentity);
     };
 
-    const margin = { top: 20, bottom: 20, left: 20, right: 20 };
-    const width = +this.width - margin.left - margin.right;
-    const height = +this.height - margin.top - margin.bottom;
-    const timelinePos = height - 20;
+    // d3.select(this.el).select('svg').remove();
+    // const svg = d3
+    //   .select(this.el)
+    //   .append('svg')
+    //   .attr('width', +this.width)
+    //   .attr('height', +this.height);
 
-    d3.select(this.el).select('svg').remove();
-    const svg = d3
-      .select(this.el)
-      .append('svg')
-      .attr('width', +this.width)
-      .attr('height', +this.height);
+    // const extraRange = Math.round((endTime - startTime) / 40);
+    // const x = d3.scaleTime()
+    //   .domain([startTime - extraRange, endTime + extraRange])
+    //   .range([0, width]);
 
-    const extraRange = Math.round((endTime - startTime) / 40);
-    const x = d3.scaleTime()
-      .domain([startTime - extraRange, endTime + extraRange])
-      .range([0, width]);
+    // svg.append('clipPath')
+    //   .attr('id', 'rect-clip')
+    //   .append('rect')
+    //   .attr('x', 0)
+    //   .attr('y', margin.top)
+    //   .attr('width', width)
+    //   .attr('height', height)
+    //   .attr('clip-path', 'url(#ellipse-clip)')
+    //   .attr('fill', 'SteelBlue');
 
-    svg.append('clipPath')
-      .attr('id', 'rect-clip')
-      .append('rect')
-      .attr('x', 0)
-      .attr('y', margin.top)
-      .attr('width', width)
-      .attr('height', height)
-      .attr('clip-path', 'url(#ellipse-clip)')
-      .attr('fill', 'SteelBlue');
+    // const tl = svg
+    //   .append('g')
+    //   .attr('transform', `translate(${margin.left}, ${margin.top})`)
+    //   .attr('clip-path', 'url(#rect-clip)') as Selection<SVGElement, any, HTMLDivElement, any>;
 
-    const tl = svg
-      .append('g')
-      .attr('transform', `translate(${margin.left}, ${margin.top})`)
-      .attr('clip-path', 'url(#rect-clip)') as Selection<SVGElement, any, HTMLDivElement, any>;
+    // const line = tl
+    //   .append('line')
+    //   .attr('class', 'timeline')
+    //   .attr('x1', 0)
+    //   .attr('y1', timelinePos)
+    //   .attr('x2', width)
+    //   .attr('y2', timelinePos);
 
-    const line = tl
-      .append('line')
-      .attr('class', 'timeline')
-      .attr('x1', 0)
-      .attr('y1', timelinePos)
-      .attr('x2', width)
-      .attr('y2', timelinePos);
-
-    // Define the div for the tooltip
-    const tooltip = d3.select('body')
-      .append('div')
-      .attr('class', 'tooltip')
-      .style('opacity', 0);
+    // // Define the div for the tooltip
+    // const tooltip = d3.select('body')
+    //   .append('div')
+    //   .attr('class', 'tooltip')
+    //   .style('opacity', 0);
 
     const formatTime = d3.timeFormat('%H:%M:%S');
-    const markers = tl
+    d3.selectAll('circle').remove();
+    const markers = this.tl
       .append('g')
       .selectAll('circle')
       .data(dc)
@@ -156,18 +228,18 @@ export class AureliaD3TimelineCustomElement {
       .append('circle')
       .attr('class', 'circle')
       .attr('r', 10)
-      .attr('cx', d => x(d.start))
+      .attr('cx', d => this.xZoomed(d.start))
       .attr('cy', timelinePos)
       .on('mouseover', d => {
-        tooltip.transition()
+        this.tooltip.transition()
           .duration(200)
           .style('opacity', .9);
-        tooltip.html(formatTime(d.start) + '<br/>' + d.label)
+        this.tooltip.html(formatTime(d.start) + '<br/>' + d.label)
           .style('left', (currentEvent.pageX) + 'px')
           .style('top', (currentEvent.pageY - 50) + 'px');
       })
-      .on('mouseout', function (d) {
-        tooltip.transition()
+      .on('mouseout', d => {
+        this.tooltip.transition()
           .duration(500)
           .style('opacity', 0);
       });
@@ -184,7 +256,7 @@ export class AureliaD3TimelineCustomElement {
       });
     // .on('dragend', null);
 
-    const curTimeline = tl
+    const curTimeline = this.tl
       .append('line')
       .attr('class', 'active-time')
       .attr('x1', Math.round(width / 2))
@@ -193,40 +265,40 @@ export class AureliaD3TimelineCustomElement {
       .attr('y2', timelinePos + 10)
       .call(drag);
 
-    const xAxis = d3.axisBottom(x)
-      .ticks(width / 100)
-      .tickFormat(this.multiTimeFormat);
+    // this.xAxis = d3.axisBottom(this.x)
+    //   .ticks(width / 100)
+    //   .tickFormat(this.multiTimeFormat);
 
-    // const view = svg.append('rect')
-    //   .attr('class', 'view')
-    //   .attr('x', 0.5)
-    //   .attr('y', 0.5)
-    //   .attr('width', width - 1)
-    //   .attr('height', height - 1);
+    // // const view = svg.append('rect')
+    // //   .attr('class', 'view')
+    // //   .attr('x', 0.5)
+    // //   .attr('y', 0.5)
+    // //   .attr('width', width - 1)
+    // //   .attr('height', height - 1);
 
-    const gX = tl.append('g')
-      .attr('class', 'axis axis-x')
-      .attr('transform', `translate(0, ${height})`)
-      .call(xAxis);
+    // const gX = this.tl.append('g')
+    //   .attr('class', 'axis axis-x')
+    //   .attr('transform', `translate(0, ${height})`)
+    //   .call(this.xAxis);
 
-    d3.select('button')
-      .on('click', resetted);
+    // d3.select('button')
+    //   .on('click', resetted);
 
     const zoomed = () => {
       // view.attr('transform', currentEvent.transform);
-      const xZoomed = currentEvent.transform.rescaleX(x);
-      gX.call(xAxis.scale(xZoomed));
+      this.xZoomed = currentEvent.transform.rescaleX(this.x);
+      this.gX.call(this.xAxis.scale(this.xZoomed));
       // markers.attr('transform', `translate(${currentEvent.transform.x}, 0) scale(${currentEvent.transform.k}, 1)`);
-      svg
+      this.svg
         .selectAll('circle')
-        .attr('cx', d => xZoomed(d.start));
+        .attr('cx', d => this.xZoomed(d.start));
     };
     const zoom = d3.zoom()
       // .scaleExtent([1, 40])
       // .translateExtent([[-100, -100], [width + 90, height + 100]])
       .on('zoom', zoomed) as ZoomBehavior<SVGElement, any>;
 
-    svg.call(zoom);
+    this.svg.call(zoom);
   }
 
   private multiTimeFormat(date: Date) {
