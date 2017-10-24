d87c018b08ed2718055ffa7064c172cbcdd360da
Erik Vullings <erik.vullings@gmail.com>
2017-10-24 12:58:08 +0200

    FIX issue with default markers not being rendered     // https://github.com/PaulLeCam/react-leaflet/issues/255#issuecomment-261904061

--- scenarist/src/components/aurelia-leaflet/aurelia-leaflet.ts
+++ scenarist/src/components/aurelia-leaflet/aurelia-leaflet.ts
@@ -53,11 +53,20 @@ export class AureliaLeafletCustomElement {
     });
 
     this.eventAggregator.subscribe('LayersChanged', (data: any) => {
-        this.layersChanged(data, null);
+      this.layersChanged(data, null);
     });
 
     this.mapOptions = defaultMapOptions;
     this.layers = defaultLayers;
+
+    // FIX issue with default markers not being rendered
+    // https://github.com/PaulLeCam/react-leaflet/issues/255#issuecomment-261904061
+    this.L.Icon.Default.imagePath = '.';
+    this.L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    });
   }
 
   public attached() {
